e6ad7e60a96e8a59f6a83ee58192e25c5ff7223c
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-10-15 17:06:07 -0400

    enhance Enemy interface with cloning and action methods

--- src/main/java/pro/elevateme/Enemy.java
+++ src/main/java/pro/elevateme/Enemy.java
@@ -3,4 +3,12 @@ package pro.elevateme;
 // We must extend the 'Cloneable' interface to indicate that we want to allow cloning
 // 'Cloneable' is a marker interface, it does not contain any methods
 public interface Enemy extends Cloneable {
+
+    Enemy clone();          // Method to clone the object
+
+    void attack();          // Method to perform an attack
+
+    void defend();          // Method to perform a defense action
+
+    void flee();            // Method to flee from battle
 }
