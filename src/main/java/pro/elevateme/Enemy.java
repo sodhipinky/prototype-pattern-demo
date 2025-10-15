27e53ba2fd9256fffabbb662c9742745eaa6ee84
Pinky Sodhi <pinky.sodhi@gmail.com>
2025-10-15 17:04:00 -0400

    add Enemy interface to support cloning functionality

---
+++ src/main/java/pro/elevateme/Enemy.java
@@ -0,0 +1,6 @@
+package pro.elevateme;
+
+// We must extend the 'Cloneable' interface to indicate that we want to allow cloning
+// 'Cloneable' is a marker interface, it does not contain any methods
+public interface Enemy extends Cloneable {
+}
